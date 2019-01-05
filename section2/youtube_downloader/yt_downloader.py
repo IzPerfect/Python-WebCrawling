e3a16e0e9de280851898323b625fc65fc7ec8ad1
IzPerfect <yjwan8390@gmail.com>
2019-01-06 03:43:51 +0900

    update comments

--- section2/youtube_downloader/yt_downloader.py
+++ section2/youtube_downloader/yt_downloader.py
@@ -15,13 +15,13 @@ cwd = os.getcwd() # 현재 경로
 --mp3_filename : 저장할 mp3파일 이름
 --only_video : 영상만 다운로드 하고 싶으면 True 입력
 
-조건 
+조건
 1. yt_downloader.py와 같은 경로에 ffmpeg.exe 실행파일이 함께 있어야함
 2. 위에 import한 라이브러리들이 깔려 있어야함
 
-ex) 
-python --yt_url www.url.com
-python --yt_url www.url.com --filename test
+ex)
+python yt_downloader.py --yt_url www.url.com
+python yt_downloader.py --yt_url www.url.com --filename test
 '''
 
 
@@ -34,6 +34,7 @@ parser.add_argument('--only_video', dest = 'only_video',default = False, help ='
 
 args = parser.parse_args()
 
+
 yt = pytube.YouTube(args.yt_url)
 videos= yt.streams.all()
 default_filename = videos[0].default_filename # 가장 화질이 좋은 첫 번째 파일 선택
@@ -43,9 +44,9 @@ ext = os.path.splitext(default_filename)[1] # 확장자명만 가져옴
 savePath = args.savePath # 다운로드 받을 경로
 if not os.path.exists(savePath): # 만약 입력한 경로가 없다면 만든 후 저장
     os.makedirs(savePath)
-	
+
 # 해당 url 다운로드
-videos[0].download(savePath) 
+videos[0].download(savePath)
 
 # 만약 영상의 filename 설정
 if args.filename: # 만약 추가적으로 다른 이름을 입력했다면
@@ -60,8 +61,8 @@ if not(args.only_video): # only video만 아닌 mp3 파일도 받고 싶다면
 		new_mp3_filename = args.mp3_filename + '.mp3'
 	else:
 		new_mp3_filename = os.path.splitext(default_filename)[0] + '.mp3'
-		
-	# cmd창에서 시키는 것과 동일	
+
+	# cmd창에서 시키는 것과 동일
 	subprocess.call(['ffmpeg', '-i',
 		os.path.join(savePath, default_filename),
 		os.path.join(savePath, new_mp3_filename)
